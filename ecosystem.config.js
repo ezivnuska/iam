1f79e2804f9b2afee9d3a3e961dfa572d792499a
Eric Zivnuska <ezivnuska@gmail.com>
2025-05-06 13:07:40 -0700

    added ecosystem config and approved sharp build scripts

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+// ecosystem.config.js
+
+module.exports = {
+    apps: [
+		{
+			name: 'iam-backend',
+			script: 'apps/backend/build/index.js',
+			interpreter: `${process.env.HOME}/.nvm/versions/node/v22.14.0/bin/node`,
+			env: {
+				NODE_ENV: 'production'
+			}
+		}
+    ]
+}  
