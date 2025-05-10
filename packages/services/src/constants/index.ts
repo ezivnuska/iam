bf0991d7ff374c946949a4869b73f3a8f7f8ca7f
Eric Zivnuska <ezivnuska@gmail.com>
2025-05-10 02:37:21 -0700

    modified log

--- packages/services/src/constants/index.ts
+++ packages/services/src/constants/index.ts
@@ -1,4 +1,5 @@
 // packages/services/src/constants/index.ts
 
 export const apiBaseUrl = process.env.API_PATH ?? 'https://iameric.me/api'
+console.log('apiBaseUrl', apiBaseUrl)
 export const TOKEN_KEY = 'accessToken'
