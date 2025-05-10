b701554a85da16e86b77a869833e1fc2607a6e76
Eric Zivnuska <ezivnuska@gmail.com>
2025-05-10 03:07:27 -0700

    added ecosystem config to tracked files

---
+++ ecosystem.config.js
@@ -0,0 +1,28 @@
+// ecosystem.config.js
+
+module.exports = {
+    apps: [
+		{
+			name: 'iam-backend',
+			script: 'apps/backend/build/index.js',
+			interpreter: `${process.env.HOME}/.nvm/versions/node/v22.14.0/bin/node`,
+			env: {
+				NODE_ENV: 'development',
+			},
+			env_production: {
+				NODE_ENV: 'production',
+			},
+		},
+		{
+			name: 'iam-webhook',
+			script: './scripts/webhook.js',
+			interpreter: `${process.env.HOME}/.nvm/versions/node/v22.14.0/bin/node`,
+			env: {
+				NODE_ENV: 'development',
+			},
+			env_production: {
+				NODE_ENV: 'production',
+			},
+		},
+    ],
+}
