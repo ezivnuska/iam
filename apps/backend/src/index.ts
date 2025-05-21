cd7d0bd6e99cdb82e58849d411917991fd54f62d
Eric Zivnuska <ezivnuska@gmail.com>
2025-05-21 01:03:32 -0700

    added mongo uri log

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -75,6 +75,7 @@ io.on('connection', (socket) => {
 
 const start = async () => {
 	try {
+		console.log('MONGO_URI', process.env.MONGO_URI)
 		await mongoose.connect(process.env.MONGO_URI!)
 		console.log('MongoDB connected')
         
