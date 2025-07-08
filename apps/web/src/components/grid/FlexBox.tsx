f7558ba2c242a69809b8d6ad59b62a51ff35699d
Eric Zivnuska <ezivnuska@gmail.com>
2025-07-08 02:40:56 -0700

    fixed commented path typo

--- apps/web/src/components/grid/FlexBox.tsx
+++ apps/web/src/components/grid/FlexBox.tsx
@@ -1,4 +1,4 @@
-// apps/web/src/components/layout/FlexBox.tsx
+// apps/web/src/components/grid/FlexBox.tsx
 
 import React from 'react'
 import { View, ViewStyle, StyleSheet, StyleProp } from 'react-native'
