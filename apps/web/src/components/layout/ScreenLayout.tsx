df9a4a26e564ae944d20bda0f93b989560a1976d
Eric Zivnuska <ezivnuska@gmail.com>
2025-07-13 07:50:44 -0700

    added safeAreaInsets

--- apps/web/src/components/layout/ScreenLayout.tsx
+++ apps/web/src/components/layout/ScreenLayout.tsx
@@ -7,6 +7,7 @@ import { MAX_WIDTH } from './constants'
 import type { ScreenLayoutProps } from '@/types'
 import { useDeviceInfo, useTheme } from '@/hooks'
 import { resolveResponsiveProp } from '@iam/theme'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 export const ScreenLayout: React.FC<ScreenLayoutProps> = ({ nav, children }) => {
 	const { theme } = useTheme()
@@ -14,13 +15,20 @@ export const ScreenLayout: React.FC<ScreenLayoutProps> = ({ nav, children }) => 
     const isLandscape = orientation === 'landscape'
     const paddingHorizontal = resolveResponsiveProp({ xs: 10, sm: 12, md: 18, lg: 24 })
     const paddingVertical = resolveResponsiveProp({ xs: 4, sm: 8, md: 12, lg: 24 })
-
+    const insets = useSafeAreaInsets()
 	return (
         <FlexBox
             flex={1}
             direction={isLandscape ? 'row' : 'column'}
             align={isLandscape ? 'flex-start' : 'center'}
-            style={{ alignItems: 'stretch', backgroundColor: theme.colors.background }}
+            style={{
+                alignItems: 'stretch',
+                backgroundColor: theme.colors.background,
+                paddingTop: insets.top,
+                paddingBottom: insets.bottom,
+                paddingLeft: insets.left,
+                paddingRight: insets.right,
+            }}
         >
             <Header>
                 {nav}
